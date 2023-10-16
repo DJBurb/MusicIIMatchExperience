3ca3a5d40afe9b9cba047db1cc606ed4e3199c99
Jason Rodgers <ja_rod@hotmail.com>
2023-10-16 18:15:57 -0500

    add menu and add the option to return to a new game or to the main menu

--- src/app/level1/level1.component.ts
+++ src/app/level1/level1.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { Results } from '../models/results.model';
 import { Router } from '@angular/router';
+import { SubLevel } from '../models/sublevel.model';
 
 @Component({
   selector: 'app-level1',
@@ -9,13 +10,20 @@ import { Router } from '@angular/router';
 })
 export class Level1Component implements OnInit {
 
-  numberOfQuestions: number = 20;
+  numberOfQuestions: number = 10;
   answerResultState: number= 2;
   makeResultsVisible: boolean;
+  subLevel: number;
   results: Results;
-  constructor(private router: Router) { }
+  constructor(private router: Router) {
+     let sub =this.router.getCurrentNavigation()?.extras.state;
+     if(sub){
+      this.subLevel = (sub as SubLevel).subLevel;
+    }
+   }
 
   ngOnInit(): void {
+
   }
 
   acceptAnswerResult(answerResult:boolean){
@@ -34,6 +42,15 @@ export class Level1Component implements OnInit {
   showResults(results:Results){
     this.results = results;
     this.makeResultsVisible= true;
+  }
+
+  tryAgain(){
+    this.makeResultsVisible= false;
+    this.router.navigate(['level1'], { state: { subLevel: this.subLevel } });
+   }
+
+  goToMenu(){
+    this.router.navigateByUrl('menu');
   }
 
 }
